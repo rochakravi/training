1befc51385535ad717397cf319a97b9b9df23204
ravi <rochak.ravi@gmail.com>
2020-08-06 11:21:58 +0530

    card

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,23 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div disabled>
+        <h4>React Training</h4>
+        <p>Lets Check</p>
+      </div>
+    );
   }
 }
 
 export default App;
+
+// 1. style
+
+// 2 . style with const
+
+// 4. className Style
+
+// 4. className with external css
+
+// 5. Styled-components =>
---
+++ src/components/functionall/react.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div disabled>
+      <h4>React Training</h4>
+      <p>Lets Check</p>
+    </div>
+  );
+};
