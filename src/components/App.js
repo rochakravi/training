d10de527e8174916f7bed0009a5e91969e0b73da
Shashank <baliyan.shashank89@gmail.com>
2020-08-11 14:45:56 +0530

    form

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 //import './design.css';
 import styles from './mystyle.module.css';
+import Form from './form';
+
 class App extends Component {
   render() {
 /*    const mystyle = {
@@ -12,31 +14,9 @@ class App extends Component {
       width: "350px",
       textalign: "center"
     };*/
-    function myfunction() {
-      alert("Your form has been submitted!");
-    }
     return (
       <div className={styles.bigblue}>
-        <form>
-          <h1 style={{backgroundcColor: "#330000"}}>Enter your details for React Training</h1>
-          <p>Name:</p>  
-          <input
-            type="text"
-          />
-          <p>Experience:</p>
-          <input
-            type="text"
-          />
-          <p>Contact No:</p> 
-          <input
-            type="text"
-          />
-          <p>Email Id:</p>  
-          <input
-            type="text"
-          />
-          <p><button onClick = {myfunction}>Submit</button></p>
-        </form>
+        <Form></Form>
       </div>
     );
   }
